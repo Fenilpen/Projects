21cd6de31c6aafc137b536068a2e113b8798acd1
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-07-02 18:58:07 +0530

    Update script.js

--- 02-weather/script.js
+++ 02-weather/script.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', ()=>{
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById('weather-info')
+    const cityNameDisplay = document.getElementById('city-name')
+    const tempratureDisplay = document.getElementById('temprature')
+    const descriptionDisplay = document.getElementById('description')
+    const errorMessage = document.getElementById('error-massage')
+})
