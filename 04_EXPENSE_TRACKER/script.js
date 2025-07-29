91aa647e69bdf13121ebf651fe9ba0c19f53083f
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-07-29 18:04:45 +0530

    Update script.js

--- 04_EXPENSE_TRACKER/script.js
+++ 04_EXPENSE_TRACKER/script.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
-  
+let expenseForm = document.getElementById("expense-form")
+let expenseName = document.getElementById("expense-name")
+let expenseAmount = document.getElementById("expense-amount")
+let expenseList = document.getElementById("expense-list")
+let totalAmountDisplay = document.getElementById("total-amount")
+
+
 }
