456264d8865c78f3a695c62c8369c1b1734efdf8
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-07-29 18:07:19 +0530

    Update script.js

--- 04_EXPENSE_TRACKER/script.js
+++ 04_EXPENSE_TRACKER/script.js
@@ -6,5 +6,29 @@ let expenseAmount = document.getElementById("expense-amount")
 let expenseList = document.getElementById("expense-list")
 let totalAmountDisplay = document.getElementById("total-amount")
 
+let expenses = []
+  
+expenseForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    console.log("Form submitted"); 
+    let name = expenseName.value.trim()
+    let amount = parseFloat(expenseAmount.value.trim())
 
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+        const newExpense = {
+            id: Date.now(),
+            name: name,
+            amount: amount
+        }
+        expenses.push(newExpense)
+        saveExpensesToLocal()
+        renderDisplay(expenses)
+        
+
+    }else {
+        alert("please enter the correct detail")
+    }
+    
+})
+  
 }
