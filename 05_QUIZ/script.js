1f05a1a134367623ec1efc5a29bb5851feea1533
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-08 18:08:37 +0530

    Update script.js

--- 05_QUIZ/script.js
+++ 05_QUIZ/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded",()=>{
 
+  const questions = [
+
+    {
+      question: "what is the capital of Paris?",
+      choices: ["paris","london","Berlin","Madrid"],
+      answer: "Paris"
+    },
+    {},
+    {}
+  ] 
+
+
+
+  
+}
